565eed3e998d32b8c6618de86a1dae3f63e29e4f
hzlloni <jhw030329@gmail.com>
2023-11-17 21:41:38 +0900

    Initial commit

---
+++ src/main/java/com/example/project4/util/JDBCUtill.java
@@ -0,0 +1,24 @@
+package com.example.project4;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class JDBCUtill {
+    public static Connection getConnection(){
+        Connection con=null;
+        try{
+            Class.forName("org.mariadb.jdbc.Driver");
+            con= DriverManager.getConnection("jdbc:mariadb://walab.handong.edu:3306/p232_22200658","p232_22200658","Faitu5");
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        return con;
+    }
+//	public static void main(String ars[]) {
+//		Connection conn = getConnection();
+//		if(conn != null)
+//			System.out.println("DB 연결됨!");
+//		else
+//			System.out.println("DB 연결중 오류 !");
+//	}
+}
